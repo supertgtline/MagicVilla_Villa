77a69a845dd601fecab20f3e4e6ad8e1768187ae
giangthao <thaogiang.90@gmail.com>
2023-08-26 12:00:33 +0700

    Status Code in Enpoints

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -10,15 +10,25 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
         // When user input id here so the GetVilla will be required id
         [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id)
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.First(u => u.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return  Ok(villa);
         }
     }
 }
