25803459e34a830bda1a7a4e019d96dc9b8fcaa8
giangthao <thaogiang.90@gmail.com>
2023-08-30 21:38:40 +0700

    Session 6: 54 Villa Number Models
    Session 6: 54 Villa Number Models

---
+++ MagicVilla_VillaAPI/Models/Dto/VillaNumberDTO.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class VillaNumberDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
