322502d3a1ec702a9eeea426ea4c848f901f5ef7
giangthao <thaogiang.90@gmail.com>
2023-09-01 13:59:40 +0700

    Session 7: 60 Add Auto Mapper

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,8 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using MagicVilla_Web;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Add Services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
