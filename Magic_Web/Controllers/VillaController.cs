1b2e135f7f19f714b8f0a441770d62fbe753975e
giangthao <thaogiang.90@gmail.com>
2023-09-02 17:31:16 +0700

    Session 7 : 65 Display all Villa List - Crea Index villa UI

--- Magic_Web/Controllers/VillaController.cs
+++ Magic_Web/Controllers/VillaController.cs
@@ -1,12 +1,34 @@
+using AutoMapper;
+using Magic_Web.Models;
+using Magic_Web.Models.Dto;
+using Magic_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
-namespace Magic_Web.Controllers;
-
-public class VillaController : Controller
+namespace Magic_Web.Controllers
 {
-    // GET
-    public IActionResult Index()
+    public class VillaController : Controller
     {
-        return View();
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+        // GET
+        [HttpGet]
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
     }
 }
