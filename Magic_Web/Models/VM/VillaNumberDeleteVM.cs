341caa32eef8d9f9bbbc2b440bd561ea48f2ecda
giangthao <thaogiang.90@gmail.com>
2023-09-09 18:45:07 +0700

    Session 7: 82 Update and Delete Action Method for Villa Number

---
+++ Magic_Web/Models/VM/VillaNumberDeleteVM.cs
@@ -0,0 +1,16 @@
+using Magic_Web.Models.Dto;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Magic_Web.Models.VM;
+
+public class VillaNumberDeleteVM
+{
+    public VillaNumberDeleteVM()
+    {
+        VillaNumber = new VillaNumberDTO();
+    }
+    public VillaNumberDTO VillaNumber { get; set; }
+    [ValidateNever]
+    public IEnumerable<SelectListItem> VillaList { get; set; }
+}
---
+++ Magic_Web/Models/VM/VillaNumberUpdateVM.cs
@@ -0,0 +1,16 @@
+using Magic_Web.Models.Dto;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Magic_Web.Models.VM;
+
+public class VillaNumberUpdateVM
+{
+    public VillaNumberUpdateVM()
+    {
+        VillaNumber = new VillaNumberUpdateDTO();
+    }
+    public VillaNumberUpdateDTO VillaNumber { get; set; }
+    [ValidateNever]
+    public IEnumerable<SelectListItem> VillaList { get; set; }
+}
