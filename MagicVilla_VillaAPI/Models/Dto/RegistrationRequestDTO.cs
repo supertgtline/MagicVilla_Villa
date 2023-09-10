c566f0b6ad055aab9a70d50b824aec3348ba9393
giangthao <thaogiang.90@gmail.com>
2023-09-10 15:25:56 +0700

    Update set to Registeraion DTO

--- MagicVilla_VillaAPI/Models/Dto/RegistrationRequestDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace MagicVilla_VillaAPI.Models.Dto;
-
-public class RegistrationRequestDTO
-{
-    public string UserName { get; set; }
-    public string Name { get; }
-    public string Password { get; }
-    public string Role { get; }
-}
