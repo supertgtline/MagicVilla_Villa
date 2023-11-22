33af7d3e6d4fa6ac6a66ecea2e1898f83da7ade7
giangthao <thaogiang.90@gmail.com>
2023-11-22 18:12:56 +0700

    Session 2 9. Load Place hold image

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -19,67 +19,66 @@ namespace MagicVilla_VillaAPI.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Villa>().HasData(
-                new Villa()
+                new Villa
                 {
                     Id = 1,
                     Name = "Royal Villa",
-                    Details =
-                        "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa3.jpg",
+                    Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                    ImageUrl = "https://placehold.co/600x401",
                     Occupancy = 4,
                     Rate = 200,
                     Sqft = 550,
                     Amenity = "",
-                    CreatedDate = DateTime.Today
+                    CreatedDate = new DateTime(2000, 01, 01) 
                 },
-                new Villa
-                {
-                    Id = 2,
-                    Name = "Premium Pool Villa",
-                    Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa1.jpg",
-                    Occupancy = 4,
-                    Rate = 300,
-                    Sqft = 550,
-                    Amenity = "",
-                    CreatedDate = DateTime.Today
-                },
-                new Villa
-                {
-                    Id = 3,
-                    Name = "Luxury Pool Villa",
-                    Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa4.jpg",
-                    Occupancy = 4,
-                    Rate = 400,
-                    Sqft = 750,
-                    Amenity = "",
-                    CreatedDate = DateTime.Today
-                },
-                new Villa
-                {
-                    Id = 4,
-                    Name = "Diamond Villa",
-                    Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa5.jpg",
-                    Occupancy = 4,
-                    Rate = 550,
-                    Sqft = 900,
-                    Amenity = "",
-                    CreatedDate = DateTime.Today
-                },
-                new Villa
-                {
-                    Id = 5,
-                    Name = "Diamond Pool Villa",
-                    Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                    ImageUrl = "https://dotnetmastery.com/bluevillaimages/villa2.jpg",
-                    Occupancy = 4,
-                    Rate = 600,
-                    Sqft = 1100,
-                    Amenity = "",
-                    CreatedDate = DateTime.Today
-                });
+              new Villa
+              {
+                  Id = 2,
+                  Name = "Premium Pool Villa",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://placehold.co/600x402",
+                  Occupancy = 4,
+                  Rate = 300,
+                  Sqft = 550,
+                  Amenity = "",
+                  CreatedDate = new DateTime(2000, 01, 01)
+              },
+              new Villa
+              {
+                  Id = 3,
+                  Name = "Luxury Pool Villa",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://placehold.co/600x403",
+                  Occupancy = 4,
+                  Rate = 400,
+                  Sqft = 750,
+                  Amenity = "",
+                  CreatedDate = new DateTime(2000, 01, 01)
+              },
+              new Villa
+              {
+                  Id = 4,
+                  Name = "Diamond Villa",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://placehold.co/600x404",
+                  Occupancy = 4,
+                  Rate = 550,
+                  Sqft = 900,
+                  Amenity = "",
+                  CreatedDate = new DateTime(2000, 01, 01)
+              },
+              new Villa
+              {
+                  Id = 5,
+                  Name = "Diamond Pool Villa",
+                  Details = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                  ImageUrl = "https://placehold.co/600x405",
+                  Occupancy = 4,
+                  Rate = 600,
+                  Sqft = 1100,
+                  Amenity = "",
+                  CreatedDate = new DateTime(2000, 01, 01)
+              });
         }
     }
 }
