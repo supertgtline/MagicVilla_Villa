430d605a3b53726205f604e235d0e08a21169f9b
giangthao <thaogiang.90@gmail.com>
2023-11-25 11:07:26 +0700

    Session 3: 17 How handle form file in Base Servie

--- Magic_Web/Models/APIRequest.cs
+++ Magic_Web/Models/APIRequest.cs
@@ -8,4 +8,5 @@ public class APIRequest
     public string Url { get; set; }
     public object Data { get; set; }
     public string Token { get; set; }
+    public ContentType ContentType { get; set; } = ContentType.Json;
 }
