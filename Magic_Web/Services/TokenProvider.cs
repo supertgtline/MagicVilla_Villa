cd63b6793079a927db325ae6805663e4a4934828
giangthao <thaogiang.90@gmail.com>
2023-11-25 20:00:32 +0700

    Session 4: 26 Implement Token Provider

--- Magic_Web/Services/TokenProvider.cs
+++ Magic_Web/Services/TokenProvider.cs
@@ -1,6 +1,42 @@
+using Magic_Web.Services.IServices;
+using MagicVilla_Utility;
+using MagicVilla_VillaAPI.Models.Dto;
+
 namespace Magic_Web.Services;
 
-public class TokenProvider
+public class TokenProvider : ITokenProvider
 {
-    
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public TokenProvider(IHttpContextAccessor contextAccessor)
+    {
+        _httpContextAccessor = contextAccessor;
+    }
+    public void SetToken(TokenDTO tokenDTO)
+    {
+        var cookieOptions = new CookieOptions { Expires = DateTime.UtcNow.AddDays(60) };
+        _httpContextAccessor.HttpContext?.Response.Cookies.Append(SD.AccessToken,tokenDTO.Token, cookieOptions);
+    }
+
+    public TokenDTO GetToken()
+    {
+        try
+        {
+            bool hasAccessToken =
+                _httpContextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.AccessToken, out string accessToken);
+            TokenDTO tokenDTO = new()
+            {
+                Token = accessToken
+            };
+            return hasAccessToken ? tokenDTO : null;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    public void ClearToken()
+    {
+        _httpContextAccessor.HttpContext?.Response.Cookies.Delete(SD.AccessToken);
+    }
 }
