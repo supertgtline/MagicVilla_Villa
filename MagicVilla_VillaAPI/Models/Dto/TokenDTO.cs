2535e65f8360a747a146584600c6be6ed26d8442
giangthao <thaogiang.90@gmail.com>
2023-11-26 07:40:11 +0700

    Session 4: 34 Separating out access token generator call

--- MagicVilla_VillaAPI/Models/Dto/TokenDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/TokenDTO.cs
@@ -2,5 +2,5 @@ namespace MagicVilla_VillaAPI.Models.Dto;
 
 public class TokenDTO
 {
-    public string Token { get; set; }
+    public string AccessToken { get; set; }
 }
--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -52,29 +52,16 @@ public class UserRepository : IUserRepository
         {
             return new TokenDTO()
             {
-                Token = ""
-                
+                AccessToken = ""
             };
         }
 
-        // if user was found generate JWT Token
-        var roles = await _userManager.GetRolesAsync(user);
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(secretKey);
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new(ClaimTypes.Name, user.Id.ToString()),
-                new(ClaimTypes.Role, roles.FirstOrDefault() ?? throw new InvalidOperationException())
-            }),
-            Expires = DateTime.UtcNow.AddDays(7),
-            SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-        };
-        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var accessToken = await GetAccessToken(user);
+        
         TokenDTO tokenDto = new TokenDTO()
         {
-            Token = tokenHandler.WriteToken(token)// Role = roles.FirstOrDefault()
+            AccessToken = accessToken,
+                
         };
         return tokenDto;
     }
@@ -108,5 +95,27 @@ public class UserRepository : IUserRepository
 
         return new UserDTO();
 
+    }
+    private async Task<string> GetAccessToken(ApplicationUser user)
+    {
+        //if user was found generate JWT Token
+        var roles = await _userManager.GetRolesAsync(user);
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.UserName.ToString()),
+                new Claim(ClaimTypes.Role, roles.FirstOrDefault())
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var tokenStr = tokenHandler.WriteToken(token);
+        return tokenStr;
     }
 }
